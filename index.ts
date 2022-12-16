7c84247f1a00f157071c3a9b95729b7a641aff18
student-ops <gogakuec@gmail.com>
2022-12-16 13:14:05 +0900

    add mongo docker compose-file

--- index.ts
+++ index.ts
@@ -3,28 +3,11 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
+    // Connect the client
     await prisma.$connect()
-
-    await prisma.user.create({
-        data: {
-            name: "Rich",
-            email: "hello@prisma.com",
-            posts: {
-                create: {
-                    title: "My first post",
-                    body: "Lots of really interesting stuff",
-                    slug: "my-first-post",
-                },
-            },
-        },
-    })
-
-    const allUsers = await prisma.user.findMany({
-        include: {
-            posts: true,
-        },
-    })
-    console.dir(allUsers, { depth: null })
+    // ... you will write your Prisma Client queries here
+    const allUsers = await prisma.user.findMany()
+    console.log(allUsers)
 }
 
 main()
