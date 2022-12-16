a34aa367353930fa6de69ce3adfaef2d42b3d935
student-ops <gogakuec@gmail.com>
2022-12-16 14:11:34 +0900

    spalate ts files

--- null-fetch.ts
+++ null-fetch.ts
@@ -4,20 +4,13 @@ const prisma = new PrismaClient()
 
 async function main() {
     await prisma.$connect()
-
-    await prisma.user.create({
-        data: {
-            name: "Rich",
-            email: "hello@prisma.com",
-            posts: {
-                create: {
-                    title: "My first post",
-                    body: "Lots of really interesting stuff",
-                    slug: "my-first-post",
-                },
-            },
-        },
-    })
+    async function main() {
+        // Connect the client
+        await prisma.$connect()
+        // ... you will write your Prisma Client queries here
+        const allUsers = await prisma.user.findMany()
+        console.log(allUsers)
+    }
 
     const allUsers = await prisma.user.findMany({
         include: {
